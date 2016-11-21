01062fc127c9608e6bfbe3610bb148d45529f9ab
Andrew Ang <ang.kunanit@gmail.com>
2016-11-21 16:15:05 -0500

    Add apply methods to operations

--- gdoc_revisions/operations.py
+++ gdoc_revisions/operations.py
@@ -10,6 +10,9 @@ class Operation(object):
         '''
         self.raw = operation_raw
 
+    def apply(document):
+        pass
+
 
 def getOperation(operation_raw):
     '''
@@ -35,6 +38,13 @@ class InsertString(Operation):
         self.start_index = operation_raw['ibi']
         self.type = 'insert string'
 
+    def apply(document):
+        '''
+        insert string into document at specified index
+        '''
+        for i,char in enumerate(string):
+            document.insert(self.start_index+i, self.string)
+
 
 class DeleteString(Operation):
     '''
@@ -45,6 +55,12 @@ class DeleteString(Operation):
         self.start_index = operation_raw['si']
         self.end_index = operation_raw['ei']
         self.type = 'delete string'
+
+    def apply(document):
+        '''
+        Delete string from document between specified indices
+        '''
+        del document[self.start_index:self.end_index]
         
 
 class MultiOperation(Operation):
@@ -57,3 +73,10 @@ class MultiOperation(Operation):
         self.suboperations = [getOperation(x) for x in operation_raw['mts']]
         self.type = 'multiple operations'
 
+    def apply(document):
+        '''
+        Apply each of the suboperations comprising the Multioperation
+        '''
+        for suboperation in self.suboperations:
+            self.suboperation.apply(document)
+            
