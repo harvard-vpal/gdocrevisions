1021f1b13e3c9acb67df26fb55d370018bcecb8e
Andrew Ang <ang.kunanit@gmail.com>
2018-04-04 14:41:33 -0400

    Use google-auth instead of oauth2client for credentials

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 from setuptools import setup
 
-setup(name='gdocrevisions',
-      version='0.5.1',
-      description='Package for downloading and analyzing google doc revision history data.',
-      url='https://github.com/harvard-vpal/gdocrevisions',
-      author='Andrew Ang',
-      author_email='andrew_ang@harvard.edu',
-      license='MIT',
-      packages=['gdocrevisions'],
-      install_requires=[
-      	'oauth2client',
-      	'google-api-python-client',
-      ],
+setup(
+    name='gdocrevisions',
+    version='0.5.1',
+    description='Package for downloading and analyzing google doc revision history data.',
+    url='https://github.com/harvard-vpal/gdocrevisions',
+    author='Andrew Ang',
+    author_email='andrew_ang@harvard.edu',
+    license='MIT',
+    packages=['gdocrevisions'],
+    install_requires=[
+        'google-auth==1.4.1',
+        'google-api-python-client==1.6.6',
+        'google-api-python-client==1.6.6',
+        'google-auth-httplib2==0.0.3',
+    ],
 )
