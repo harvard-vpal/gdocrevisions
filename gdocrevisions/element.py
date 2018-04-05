920df2498baac4f4a65035b206485a464e7f2258
Andrew Ang <ang.kunanit@gmail.com>
2018-04-05 10:51:55 -0400

    Indentation formatting

--- gdocrevisions/element.py
+++ gdocrevisions/element.py
@@ -1,50 +1,50 @@
 class Element(object):
-	"""
-	Base Element class. 
-	Is a list element in a document's content attribute.
-	Associated with a revision
-	"""
-	__slots__ = ('revision')
-	def __init__(self, revision):
-		self.revision = revision
+    """
+    Base Element class. 
+    Is a list element in a document's content attribute.
+    Associated with a revision
+    """
+    __slots__ = ('revision')
+    def __init__(self, revision):
+        self.revision = revision
 
-	def render(self):
-		return ""
+    def render(self):
+        return ""
 
 
 class Character(Element):
-	"""
-	Character element
-	Represents a single character
-	"""
-	__slots__ = ('char', 'revision_insert','revision_delete')
-	def __init__(self, char, revision):
-		# character string
-		self.char = char
-		# revision in which element is created
-		self.revision_insert = revision
-		# placeholder for revision in which element is deleted
-		self.revision_delete = None
+    """
+    Character element
+    Represents a single character
+    """
+    __slots__ = ('char', 'revision_insert','revision_delete')
+    def __init__(self, char, revision):
+        # character string
+        self.char = char
+        # revision in which element is created
+        self.revision_insert = revision
+        # placeholder for revision in which element is deleted
+        self.revision_delete = None
 
-	@property
-	def revision(self):
-		"""
-		Alias for revision_insert attribute
-		"""
-		return self.revision_insert
+    @property
+    def revision(self):
+        """
+        Alias for revision_insert attribute
+        """
+        return self.revision_insert
 
-	def render(self):
-		"""
-		Render this element, typically called by content.render()
-		"""
-		return self.char
+    def render(self):
+        """
+        Render this element, typically called by content.render()
+        """
+        return self.char
 
 
 class EndOfBody(Element):
-	"""
-	Dummy element indicating end of document body
-	Separates main content from footnote text
-	"""
-	__slots__ = ()
-	def __init__(self):
-		pass
+    """
+    Dummy element indicating end of document body
+    Separates main content from footnote text
+    """
+    __slots__ = ()
+    def __init__(self):
+        pass
--- gdocrevisions/session.py
+++ gdocrevisions/session.py
@@ -1,17 +1,15 @@
 class Session(object):
-	"""
-	Session class
-	"""
-	def __init__(self, revisions):
-		# list of revisions
-		self.revisions = revisions
-		# session id
-		self.session_id = self.revisions[0]
-		# time of first revision
-		self.start_time = self.revisions[0].time
-		# time of last revision
-		self.end_time = self.revisions[-1].time if len(self.revisions)>1 else self.start_time
-		# user
-		self.user_id  = self.revisions[0].user_id
-
-
+    """
+    Session class
+    """
+    def __init__(self, revisions):
+        # list of revisions
+        self.revisions = revisions
+        # session id
+        self.session_id = self.revisions[0]
+        # time of first revision
+        self.start_time = self.revisions[0].time
+        # time of last revision
+        self.end_time = self.revisions[-1].time if len(self.revisions)>1 else self.start_time
+        # user
+        self.user_id  = self.revisions[0].user_id
