13b29d012a4fa868c5ba295ae1c93e51123364b0
Andrew Ang <ang.kunanit@gmail.com>
2018-04-06 14:54:08 -0400

    Add requests to setup requirements

--- setup.py
+++ setup.py
@@ -13,5 +13,6 @@ setup(
         'google-auth==1.4.1',
         'google-api-python-client==1.6.6',
         'google-auth-httplib2==0.0.3',  # required for using google.auth credentials with google-api-python-client
+        'requests==2.18.1',
     ],
 )
